bf4192860870f3a86e3db475d3782ef82deb21e8
Karol <kpavasl@unal.edu.co>
2023-08-17 01:35:21 -0500

    navbar created.

--- ticket-cinema-system/src/App.js
+++ ticket-cinema-system/src/App.js
@@ -1,17 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './components/navbar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Cinema System
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+          <Navbar />
+        </div>
+    );
 }
 
 export default App;
---
+++ ticket-cinema-system/src/components/navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../navbar.css';
+
+function Navbar(props) {
+    return (
+        <nav className='nav'>
+          <a href='#' className='nav__brand'>Cinema System</a>
+          <ul className='nav__menu'>
+            <li className='nav__item'><a href='#' className='nav__link'>Inicio</a></li>
+            <li className='nav__item'><a href='#' className='nav__link'>Usuarios</a></li>
+          </ul>
+          <div className='nav__toggler'>
+            <div className='line1'></div>            
+            <div className='line2'></div>
+            <div className='line3'></div>
+          </div>
+        </nav>
+    );
+}
+
+export default Navbar;
